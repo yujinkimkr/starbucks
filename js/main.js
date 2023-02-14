8e649449618024f34c6b1859635782f2fb350f7f
yujinkimkr <kyjin.dv@gmail.com>
2023-02-14 14:26:12 +0100

    Start project

---
+++ js/main.js
@@ -0,0 +1,168 @@
+const searchEl = document.querySelector(".search");
+const searchInputEl = searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function() {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener("focus", function(){
+    searchEl.classList.add("focused");
+    searchInputEl.setAttribute("placeholder", '통합검색');
+});
+
+searchInputEl.addEventListener("blur", function(){
+    searchEl.classList.remove("focused");
+    searchInputEl.setAttribute("placeholder", '');
+});
+
+
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector("#to-top");
+
+// 스크롤할 때마다 함수가 실행된다 
+// _.throttle(함수, 시간)
+window.addEventListener("scroll", _.throttle(function(){
+    if(window.scrollY > 500) {
+        // 배지 숨기기
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+        // badgeEl.style.display = 'none';
+
+        //버튼 보이기!
+        gsap.to(toTopEl, .2, {
+            x: 0 
+        });
+
+
+    } else {
+        // 배지 보이기
+        // badgeEl.style.display = 'block';
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+
+        //버튼 숨기기!
+        gsap.to(toTopEl, .2, {
+            x: 100 
+        });
+    }
+}, 300));
+
+
+toTopEl.addEventListener("click", function() {
+    gsap.to(window, .2, {
+        scrollTo: 0
+    });
+    // gsap.to(window, {scrollTo: 400});
+
+});
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function(fadeEl, index){
+    gsap.to(
+        fadeEl, 1, {
+            delay: (index + 1) * .6,
+            opacity: 1
+        }   
+    );
+});
+
+// new Swiper( 선택자, 옵션 )
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',   
+    autoplay: true,
+    loop: true
+})
+// 선택자를 인수 형태로 삽입 
+
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3, //한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10, //슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 4000 // 4초, 기본값은 3초, 3000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자 
+        clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
+new Swiper('.awards .swiper-container', {
+    // direction: 'horizontal',
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+
+let isHidePromotion = false;
+// let이기 때문에 다른 값으로 할당 될 수 있다
+promotionToggleBtn.addEventListener('click', function() {
+    // 숨겨주는 로직 작성 
+    isHidePromotion = !isHidePromotion;
+    if(isHidePromotion) {
+        // 숨김 처리
+        promotionEl.classList.add("hide");
+    } else {
+        // 출력 처리
+        promotionEl.classList.remove("hide");
+    }
+});
+
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  }
+
+function floatingObject(selector, delay, size) {
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(
+        selector, random(1.5, 2.5), {
+            y: size,
+            repeat: -1, // 무한 반복
+            yoyo: true,
+            ease: Power1.easeInOut,
+            delay: random(0, delay)
+        });
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+//실행하면서 css 선택자를 넣어줘야 한다
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function(spyEl){
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl, // 보여짐 여부를 감시할 요소
+            triggerHook: .8 // 
+        })
+        .setClassToggle(spyEl, 'show') // 토글할 요소와 클래스 이름
+        .addTo(new ScrollMagic.Controller());
+    
+});
+
+// 수정 필요!
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear();
---
+++ js/youtube.js
@@ -0,0 +1,26 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+    new YT.Player('player', { //plyaer라는 id 값을 찾음
+        videoId: 'o-Ikkh5oxuo', // 최초 재생할 유투브 영상 ID
+        playerVars: {
+            autoplay: true, // 자동 재생 유무
+            loop: true, // 반복 재생 유무 
+            playlist: 'o-Ikkh5oxuo'
+        },
+        events: {
+            onReady: function(event) {
+                event.target.mute(); // target은 재생되고 있는 영상 자체
+                // 음소거 - 기본적으로 음소거 처리함
+            }
+        }
+    });
+}
